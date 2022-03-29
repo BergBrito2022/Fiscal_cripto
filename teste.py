ace154f67db2fcf16f052bbf759cf1c0ac2e83ec
Gutemberg Brito <98509572+BergBrito2022@users.noreply.github.com>
2022-03-29 02:43:13 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,28 +5,27 @@ from sqlalchemy import create_engine
 
 
 price = requests.get('https://api.binance.com/api/v3/ticker/price') # Ativos e preços.
-lista_precos = []
+price_list = []
 
 
 if price.status_code == 200:
    data = price.json()
    for i in data:
-      lista_precos.append([i['symbol'], i['price']])
+      price_list.append([i['symbol'], i['price']])
 else:
    erro = price.raise_for_status()
    print(f'Ocorreu o seguinte erro no acesso da API: {erro}')
 
-# Transformando a lista de endereços em um DataFrame
+# Transformando price_list em um DataFrame
 
-df_lista_precos = pd.DataFrame(lista_precos, columns=['symbol', 'price'])  #Tratando Dados com pandas.
+df_price_list = pd.DataFrame(price_list, columns=['symbol', 'price'])  #Tratando Dados com pandas.
 print(df_lista_precos)
 
-
-for c in lista_precos:
+for c in price_list:
    print(c)
 
 # Preparando a conexão e gravando em uma tabela no banco de dados.
 
 db_connection = 'mysql+pymysql://user:password@host:port/database'  
 db_connection = create_engine(db_connection)
-df_lista_precos.to_sql(con=db_connection, name='enderecos', if_exists='replace', index=False)
+df_price_list.to_sql(con=db_connection, name='price_list_binanc', if_exists='replace', index=False)
